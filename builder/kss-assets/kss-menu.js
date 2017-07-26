463ac780ee872055583ab982280710449f041963
Eric Huffman <eric.huffman@mediacurrent.com>
2017-07-25 17:47:12 -0700

    Updates menu, layout styling / and js

--- builder/kss-assets/kss-menu.js
+++ builder/kss-assets/kss-menu.js
@@ -1,12 +1,67 @@
 !(function ($) {
   'use strict';
 
-  var $menuLinks = $('.kss__menu-list-item a');
+  // Section links.
+  var $sLinks = $('.kss__menu-list-item--section > .kss__menu-list-item-link');
 
-  var $contentFrame = $('.kss-layout__content-frame');
+  // Page links.
+  var $pLinks = $('.kss__menu-list-item--page > .kss__menu-list-item-link');
 
-  $menuLinks.on('click', function(event) {
-    $contentFrame.attr('src', $(this).attr('href'));
+  // Content iframe.
+  var $cFrame = $('.kss-layout__content-frame');
+
+  // Content wrapper for homepage content.
+  var $hContent = $('.kss-layout__main--home > .kss-layout__content-wrapper');
+
+  // Menu toggle button.
+  var $menuToggle = $('.kss__menu-toggle');
+
+  // Breakpoint menu button.
+  var $bpLink = $('.kss__breakpoint-menu-button');
+
+  $sLinks.on('click', function(event) {
+    var $self = $(this);
+    if ($self.hasClass('kss-open')) {
+      $self.removeClass('kss-open');
+    }
+    else {
+      $sLinks.removeClass('kss-open');
+      $self.addClass('kss-open');
+    }
+    event.preventDefault();
+  });
+
+  $pLinks.on('click', function(event) {
+    var $self = $(this);
+    $pLinks.removeClass('kss-active');
+    $cFrame.attr('src', $self.attr('href'));
+    $cFrame.addClass('kss-active');
+    $cFrame.parent().addClass('kss-active');
+    $self.addClass('kss-active');
+    $self.parents('.kss__menu-list--child')
+      .siblings('.kss__menu-list-item-link').removeClass('kss-open');
+    $menuToggle.removeClass('kss-open');
+    if (!$hContent.hasClass('kss-inactive')) {
+      $hContent.addClass('kss-inactive');
+    }
+    event.preventDefault();
+  });
+
+  $menuToggle.on('click', function(event) {
+    $(this).toggleClass('kss-open');
+  });
+
+  $bpLink.on('click', function(event) {
+    var $self = $(this);
+    $bpLink.removeClass('kss-active');
+    $self.addClass('kss-active');
+    var bpValue = $self.data('bp-value');
+    if (bpValue === 'full') {
+      $cFrame.css('width', '100%');
+    }
+    else {
+      $cFrame.css('width', bpValue);
+    }
     event.preventDefault();
   });
 
