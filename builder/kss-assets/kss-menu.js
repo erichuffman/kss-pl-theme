e35f7d3b61d0d2ef56c807ec0dad59af4f22a948
Eric Huffman <eric.huffman@mediacurrent.com>
2017-07-26 17:16:49 -0700

    Stying/markup/js updates for menus Styling updates for main content area Adds compiled CSS file

--- builder/kss-assets/kss-menu.js
+++ builder/kss-assets/kss-menu.js
@@ -2,10 +2,10 @@
   'use strict';
 
   // Section links.
-  var $sLinks = $('.kss__menu-list-item--section > .kss__menu-list-item-link');
+  var $sLinks = $('.kss-menu__list-item--section > .kss-menu__list-item-link');
 
   // Page links.
-  var $pLinks = $('.kss__menu-list-item--page > .kss__menu-list-item-link');
+  var $pLinks = $('.kss-menu__list-item--page > .kss-menu__list-item-link');
 
   // Content iframe.
   var $cFrame = $('.kss-layout__content-frame');
@@ -14,36 +14,59 @@
   var $hContent = $('.kss-layout__main--home > .kss-layout__content-wrapper');
 
   // Menu toggle button.
-  var $menuToggle = $('.kss__menu-toggle');
+  var $menuToggle = $('.kss-menu__toggle');
 
   // Breakpoint menu button.
-  var $bpLink = $('.kss__breakpoint-menu-button');
+  var $bpLink = $('.kss-breakpoint-menu__button');
 
   $sLinks.on('click', function(event) {
     var $self = $(this);
+    var $childMenu = $self.find('+ .kss-menu__list--child');
     if ($self.hasClass('kss-open')) {
       $self.removeClass('kss-open');
+      $self.attr('aria-expanded', 'false');
+      $childMenu.attr('aria-hidden', 'true');
     }
     else {
       $sLinks.removeClass('kss-open');
       $self.addClass('kss-open');
+      $self.attr('aria-expanded', 'true');
+      $childMenu.attr('aria-hidden', 'false');
     }
     event.preventDefault();
   });
 
   $pLinks.on('click', function(event) {
     var $self = $(this);
-    $pLinks.removeClass('kss-active');
+    var $parentMenu = $self.parents('.kss-menu__list--child');
+    var $parentTrigger = $parentMenu.siblings('.kss-menu__list-item-link');
+    var $bpMenu = $bpLink.parents('.kss-breakpoint-menu');
+    // Active/update iframe src.
     $cFrame.attr('src', $self.attr('href'));
     $cFrame.addClass('kss-active');
     $cFrame.parent().addClass('kss-active');
-    $self.addClass('kss-active');
-    $self.parents('.kss__menu-list--child')
-      .siblings('.kss__menu-list-item-link').removeClass('kss-open');
-    $menuToggle.removeClass('kss-open');
+    // Hide index page content if not already hidden.
     if (!$hContent.hasClass('kss-inactive')) {
       $hContent.addClass('kss-inactive');
     }
+    // Reveal breakpoint menu if not already visible.
+    if (!$bpMenu.hasClass('kss-active')) {
+      $bpMenu.addClass('kss-active');
+    }
+    // Remove open class from main menu toggle.
+    $menuToggle.removeClass('kss-open');
+    // Remove active class from all section menu links.
+    $sLinks.removeClass('kss-active');
+    // Update section link/menu.
+    $parentMenu.attr('aria-hidden', 'true');
+    $parentTrigger.removeClass('kss-open');
+    $parentTrigger.addClass('kss-active');
+    $parentTrigger.attr('aria-expanded', 'false');
+    // Remove active class from all page links.
+    $pLinks.removeClass('kss-active');
+    // Set active class to current page link.
+    $self.addClass('kss-active');
+    // Prevent default link behavior.
     event.preventDefault();
   });
 
